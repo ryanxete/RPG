d408004ca1024eba4d87372a5e1fc31026b0f562
Ryan Etedali <ryanetedali@gmail.com>
2022-07-09 23:04:49 -0500

    added more py files for each class

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-def sayhi():
-    print('say hi azizam!')
-    
+# As a developer, I want to make at least five commits on GitHub with descriptive 
+# messages. 
+# As a user, I want an engaging story to be told using print() statements. 
+# As a user, I want Hercules (and each enemy), to have health, attack power, and a 
+# List of attack names saved in a Dictionary.
+# As a user, I want the ability to select Hercules’ attack using a menu prompt.
+# As a user, I want the foe’s attack to be chosen at random.
+# As a user, I want the results of each attack to be logged in the terminal. 
+# As a developer, I want to use an Attack() function that will terminate when Hercules 
+# or an enemy’s health reaches zero. 
+# As a developer, I want my RunGame() function to call my other functions in a logical
+# order that will determine game flow.
+# As a developer, I want all of my functions to have a Single 
+# Responsibility. Remember, each function should do just one thing!
